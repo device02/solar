33eedb87cfcb67e3b11cfadf5289cdbf69d4f4d0
device02 <viccasvictor@gmail.com>
2025-09-16 21:20:25 +0100

    solar first upload

---
+++ solar.js
@@ -0,0 +1,17 @@
+
+// Scroll to Top Function
+// Scroll to top
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+
+// Scroll to bottom
+function scrollToBottom() {
+  window.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: "smooth"
+  });
+}
